be176025ae1ca9da1c87bda97c483304623a1f9c
Piotr <piotro@arg.pl>
2020-07-29 18:26:53 +0200

    Initial Project Commit

---
+++ src/main/java/org/piotr/petclinicwithspringboot/PetClinicWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.piotr.petclinicwithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/piotr/petclinicwithspringboot/PetClinicWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.piotr.petclinicwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
