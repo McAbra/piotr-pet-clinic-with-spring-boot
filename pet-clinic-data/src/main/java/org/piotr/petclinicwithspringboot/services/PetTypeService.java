a4c109ed35e763850c5f6540060157c8d03ad27a
Piotr <piotro@arg.pl>
2020-08-14 17:21:01 +0200

    Added PetTypeService Map. Closes #25

---
+++ pet-clinic-data/src/main/java/org/piotr/petclinicwithspringboot/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package org.piotr.petclinicwithspringboot.services;
+
+import org.piotr.petclinicwithspringboot.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
