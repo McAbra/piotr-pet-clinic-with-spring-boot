545d908a5c0704e15ce9d4d162b3dbf1edf0df5a
Piotr <piotro@arg.pl>
2020-08-15 12:09:28 +0200

    Added Specialties Map Impl. Closes #27

--- pet-clinic-data/src/main/java/org/piotr/petclinicwithspringboot/model/Specialty.java
+++ pet-clinic-data/src/main/java/org/piotr/petclinicwithspringboot/model/Specialty.java
@@ -1,6 +1,6 @@
 package org.piotr.petclinicwithspringboot.model;
 
-public class Speciality extends BaseEntity {
+public class Specialty extends BaseEntity {
 
     private String description;
 
---
+++ pet-clinic-data/src/main/java/org/piotr/petclinicwithspringboot/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package org.piotr.petclinicwithspringboot.services;
+
+import org.piotr.petclinicwithspringboot.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
