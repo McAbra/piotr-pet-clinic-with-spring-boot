2ccf5dcae477fe1218b6db93187f4dec004ebb1c
Piotr <piotro@arg.pl>
2020-08-15 12:01:43 +0200

    Added Pets to Owners in bootstrap data loader. Closes #29

--- pet-clinic-web/src/main/java/org/piotr/petclinicwithspringboot/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/org/piotr/petclinicwithspringboot/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package org.piotr.petclinicwithspringboot.bootstrap;
 
 import org.piotr.petclinicwithspringboot.model.Owner;
+import org.piotr.petclinicwithspringboot.model.Pet;
 import org.piotr.petclinicwithspringboot.model.PetType;
 import org.piotr.petclinicwithspringboot.model.Vet;
 import org.piotr.petclinicwithspringboot.services.PetTypeService;
@@ -8,6 +9,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.piotr.petclinicwithspringboot.services.OwnerService;
 import org.piotr.petclinicwithspringboot.services.VetService;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -30,19 +33,39 @@ public class DataLoader implements CommandLineRunner {
         PetType savedDogPetType = petTypeService.save(dog);
 
         PetType cat = new PetType();
-        dog.setName("Cat");
+        cat.setName("Cat");
         PetType savedCatPetType = petTypeService.save(cat);
 
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Janusz");
         owner1.setLastName("Gajos");
+        owner1.setAddress("Piotrkowska 1");
+        owner1.setCity("Kaszanka");
+        owner1.setPhone("125 125 147");
+
+        Pet jPet = new Pet();
+        jPet.setPetType(savedDogPetType);
+        jPet.setName("Azor");
+        jPet.setOwner(owner1);
+        jPet.setBirthDate(LocalDate.of(2015, 11, 10));
+        owner1.getPets().add(jPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Kondrad");
         owner2.setLastName("Skonieczny");
+        owner2.setAddress("Warszawska 8");
+        owner2.setCity("Kielce");
+        owner2.setPhone("879 879 987");
+
+        Pet kPet = new Pet();
+        kPet.setPetType(savedCatPetType);
+        kPet.setName("Mruk");
+        kPet.setOwner(owner2);
+        kPet.setBirthDate(LocalDate.of(2010, 11, 10));
+        owner2.getPets().add(kPet);
 
         ownerService.save(owner2);
 
