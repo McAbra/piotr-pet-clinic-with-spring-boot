9d23dc65b342adc079ed94df6cd61be43b9f9fd5
Piotr <piotro@arg.pl>
2020-08-19 17:09:35 +0200

    Owner updated with JPA annotations. Closes #32

--- pet-clinic-data/src/main/java/org/piotr/petclinicwithspringboot/model/Pet.java
+++ pet-clinic-data/src/main/java/org/piotr/petclinicwithspringboot/model/Pet.java
@@ -1,12 +1,21 @@
 package org.piotr.petclinicwithspringboot.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
-public class Pet extends BaseEntity{
+@Entity
+@Table(name = "pets")
+public class Pet extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+    @Column(name = "birt_date")
     private LocalDate birthDate;
 
     public String getName() {
