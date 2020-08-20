d19c83b8da99f7589cb2bff47eb50f8537af4185
Piotr <piotro@arg.pl>
2020-08-20 10:21:44 +0200

    added spring data jpa service for Specialty, Pet and PetType. Closes #38 Closes  #39

---
+++ pet-clinic-data/src/main/java/org/piotr/petclinicwithspringboot/services/springdatajpa/PetTypeSDJpaService.java
@@ -0,0 +1,48 @@
+package org.piotr.petclinicwithspringboot.services.springdatajpa;
+
+import org.piotr.petclinicwithspringboot.model.PetType;
+import org.piotr.petclinicwithspringboot.repositories.PetTypeRepository;
+import org.piotr.petclinicwithspringboot.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetTypeSDJpaService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeSDJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return petTypeRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petTypeRepository.deleteById(id);
+    }
+}
