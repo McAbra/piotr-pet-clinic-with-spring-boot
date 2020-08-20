c8260237b38b89cbbed022966ab2efcf144c294f
Piotr <piotro@arg.pl>
2020-08-20 12:23:39 +0200

    Added Visit Map Service . Closes #42

---
+++ pet-clinic-data/src/main/java/org/piotr/petclinicwithspringboot/services/VisitService.java
@@ -0,0 +1,6 @@
+package org.piotr.petclinicwithspringboot.services;
+
+import org.piotr.petclinicwithspringboot.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/org/piotr/petclinicwithspringboot/services/map/VisitMapService.java
@@ -0,0 +1,39 @@
+package org.piotr.petclinicwithspringboot.services.map;
+
+import org.piotr.petclinicwithspringboot.model.Visit;
+import org.piotr.petclinicwithspringboot.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+        || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
