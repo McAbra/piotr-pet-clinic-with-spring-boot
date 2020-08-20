9d15c5637b1693fea4341b3e95a93ef08c0171dc
Piotr <piotro@arg.pl>
2020-08-20 10:03:51 +0200

    added spring data jpa service for Vet. Closes #37

--- pet-clinic-data/src/main/java/org/piotr/petclinicwithspringboot/services/PetTypeService.java
+++ pet-clinic-data/src/main/java/org/piotr/petclinicwithspringboot/services/PetTypeService.java
@@ -3,4 +3,5 @@ package org.piotr.petclinicwithspringboot.services;
 import org.piotr.petclinicwithspringboot.model.PetType;
 
 public interface PetTypeService extends CrudService<PetType, Long> {
+
 }
---
+++ pet-clinic-data/src/main/java/org/piotr/petclinicwithspringboot/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,54 @@
+package org.piotr.petclinicwithspringboot.services.springdatajpa;
+
+
+import org.piotr.petclinicwithspringboot.model.Vet;
+import org.piotr.petclinicwithspringboot.repositories.VetRepository;
+import org.piotr.petclinicwithspringboot.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Vet findByLastName(String lastName) {
+        return vetRepository.findByLastName(lastName);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+}
